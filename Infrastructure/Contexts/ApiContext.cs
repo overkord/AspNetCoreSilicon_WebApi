0b0ee15ba24dc3f8de2a10d3672fd2cd68c76d67
Albin <Overkord123@hotmail.com>
2024-04-18 16:37:17 +0200

    Add project files.

---
+++ Infrastructure/Contexts/ApiContext.cs
@@ -0,0 +1,10 @@
+﻿using Infrastructure.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Contexts;
+
+public class ApiContext(DbContextOptions<ApiContext> options) : DbContext(options)
+{
+    public DbSet<SubscribersEntity> Subscribers { get; set; }
+    public DbSet<CoursesEntity> Courses { get; set; }
+}
---
+++ Infrastructure/Entities/CoursesEntity.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+
+namespace Infrastructure.Entities;
+
+public class CoursesEntity
+{
+    [Key]
+public int Id { get; set; }
+public bool IsBestSeller { get; set; }
+public string Image { get; set; } = null!;
+public string Title { get; set; } = null!;
+public string Author { get; set; } = null!;
+public string Price { get; set; } = null!;
+public string? DiscountPrice { get; set; }      
+public string Hours { get; set; } = null!;
+public string LikesInProcent { get; set; } = null!;
+public string LikesInNumbers { get; set; } = null!;
+}
